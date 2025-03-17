50ab0604be09f35532c4ea2988bbff35d4386b06
misho goshadze <mishikogoshadze28@gmail.com>
2025-03-17 14:26:16 +0400

    fix problem

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,22 @@ import TempMaxIcon from "./assets/Vector.svg"
 import windIcon from "./assets/outline (2).svg"
 import humadityIcon from "./assets/outline.svg"
 import tempMinIcon from "./assets/Vector (1).svg"
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useState } from "react"
 
-interface Types {
-  city: string;
-  current_temp: number;
-  current_weather_icon: string;
-  current_weather: string;
-  current_time: string;
-  temp_max: number;
-  temp_min: number;
-  humadity: number;
-  cloudy: number;
-  wind_speed: number;
+// interface Types {
+//   city: string;
+//   current_temp: number;
+//   current_weather_icon: string;
+//   current_weather: string;
+//   current_time: string;
+//   temp_max: number;
+//   temp_min: number;
+//   humadity: number;
+//   cloudy: number;
+//   wind_speed: number;
 
 
-}
+// }
 
 function App() {
   const [data, setData] = useState({
